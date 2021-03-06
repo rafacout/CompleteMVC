bc22d23caaace51e8180e0eb0de6cb1a5d4fbed5
Rafael Coutinho <rafacout@gmail.com>
2021-03-06 17:40:48 -0300

    First Version

---
+++ src/DevIO.Business/Models/Fornecedor.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevIO.Business.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+
+        public string Documento { get; set; }
+
+        public TipoFornecedorEnum TipoFornecedor { get; set; }
+
+        public Endereco Endereco { get; set; }
+
+        public bool Ativo { get; set; }
+
+        /*EF Relations*/
+        public IEnumerable<Produto> Produtos { get; set; }
+
+    }
+}
---
+++ src/DevIO.Business/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Models
+{
+    public class Produto : Entity
+    {
+        public Guid FornecedorId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Imagem { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+
+        /*EF Relation*/
+        public Fornecedor Fornecedor { get; set; }
+    }
+}

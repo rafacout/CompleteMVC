c496244fb4ea2e349aecca0447bc470fca73a6d0
Rafael Coutinho <rafacout@gmail.com>
2021-04-01 08:35:34 -0300

    Adjustments in update upload image

--- src/DevIO.App/Controllers/ProdutosController.cs
+++ src/DevIO.App/Controllers/ProdutosController.cs
@@ -107,13 +107,34 @@ namespace DevIO.App.Controllers
             {
                 return NotFound();
             }
+            
+            var produtoAtualizacao = await FindProduto(id);
+            produtoViewModel.FornecedorId = produtoAtualizacao.FornecedorId;
+            produtoViewModel.Imagem = produtoAtualizacao.Imagem;
 
             if (!ModelState.IsValid)
             {
                 return View(produtoViewModel);
             }
 
-            await _produtoRepository.Update(_mapper.Map<Produto>(produtoViewModel));
+            if (produtoViewModel.ImagemUpload != null)
+            {
+                var imgPrefixo = Guid.NewGuid() + "_";
+
+                if (!await UploadArquivo(produtoViewModel.ImagemUpload, imgPrefixo))
+                {
+                    return View(produtoViewModel);
+                }
+
+                produtoAtualizacao.Imagem = imgPrefixo + produtoViewModel.ImagemUpload.FileName;
+            }
+
+            produtoAtualizacao.Nome = produtoViewModel.Nome;
+            produtoAtualizacao.Descricao = produtoViewModel.Descricao;
+            produtoAtualizacao.Valor = produtoViewModel.Valor;
+            produtoAtualizacao.Ativo = produtoViewModel.Ativo;
+
+            await _produtoRepository.Update(_mapper.Map<Produto>(produtoAtualizacao));
 
             return RedirectToAction(nameof(Index));
         }
